b4172ee8b04439f93b3e4a74c644247547c824ef
CoderJoy <296351685@qq.com>
2021-07-20 00:01:24 +0800

    rm bat comment

--- start.bat
+++ start.bat
@@ -1,13 +1,12 @@
 @echo off
-:: cd /d %~dp0
-set root=%~dp0
-:: 拷贝国内pip镜像源配置
+
+set root=%cd%
+
 if not exist %appdata%\pip\pip.ini (
 mkdir %appdata%\pip
-copy %root%config\pip.ini %appdata%\pip
+copy %root%\config\pip.ini %appdata%\pip
 )
 
-:: 下载依赖并启动
+
 call pip install newspaper3k selenium bs4 jsonpath requests PySide2 urllib3==1.25.8
-call python %root%main.py
-:: pause
+call python %root%\main.py
